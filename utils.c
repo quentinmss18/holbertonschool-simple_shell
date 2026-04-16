a1c4dbc37e9c726f9ef359723544786eb78bd90c
quentinmss <quentinmoussous@gmail.com>
2026-04-16 11:46:28 +0200

    Refactor getenv and add utility functions
    Refactor environment variable functions and add utility functions.

--- utils.c
+++ utils.c
@@ -1,61 +1,43 @@
 #include "shell.h"
 
 /**
- * _getenv - Gets the value of an environment variable
- * @name: Name of the variable
- * Return: Pointer to the value, or NULL if not found
+ * free_args - Frees a null-terminated array of strings
+ * @args: The array to free
  */
-char *_getenv(const char *name)
+void free_args(char **args)
 {
 	int i = 0;
-	size_t len = strlen(name);
 
-	while (environ[i])
+	if (!args)
+		return;
+
+	while (args[i])
 	{
-		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-			return (environ[i] + len + 1);
+		free(args[i]);
 		i++;
 	}
-	return (NULL);
+	free(args);
 }
 
 /**
- * _find_path - Finds the full path of a command using PATH
- * @command: The command to find (e.g. "ls")
- * Return: Full path string, or NULL if not found
+ * is_empty - Checks if a string is empty or whitespace only
+ * @line: The string to check
+ *
+ * Return: 1 if empty/whitespace, 0 otherwise
  */
-char *_find_path(char *command)
+int is_empty(char *line)
 {
-	char *path, *path_copy, *token, *full_path;
-	struct stat st;
+	int i = 0;
 
-	if (command == NULL)
-		return (NULL);
+	if (!line)
+		return (1);
 
-	/* If command is already a full path, check if it exists */
-	if (stat(command, &st) == 0)
-		return (strdup(command));
-
-	path = _getenv("PATH");
-	if (!path || !*path)
-		return (NULL);
-
-	path_copy = strdup(path);
-	token = strtok(path_copy, ":");
-	while (token)
+	while (line[i])
 	{
-		full_path = malloc(strlen(token) + strlen(command) + 2);
-		if (!full_path)
-			break;
-		sprintf(full_path, "%s/%s", token, command);
-		if (stat(full_path, &st) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
+		if (line[i] != ' ' && line[i] != '\t' &&
+		    line[i] != '\r' && line[i] != '\n')
+			return (0);
+		i++;
 	}
-	free(path_copy);
-	return (NULL);
+	return (1);
 }
